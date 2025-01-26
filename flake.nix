818ac064aa4cc795f1d6f353c51fa4bd0f2cdba6
David Himmelstrup <david.himmelstrup@chainsafe.io>
2025-01-26 10:57:29 +0100

    move worker-build to separate repository

--- flake.nix
+++ flake.nix
@@ -3,18 +3,21 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    worker-build.url = "github:lemmih/nix-flakes?dir=worker-build";
+    wrangler.url = "github:ryand56/wrangler";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, naersk, worker-build, wrangler }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = nixpkgs.legacyPackages.${system};
         naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
+        worker-build-bin = worker-build.packages.${system}.default;
+        wrangler-bin = wrangler.packages.${system}.default;
+      in {
         # defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy wasm-pack worker-build ];
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy lld wasm-pack worker-build-bin wrangler-bin ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       }
